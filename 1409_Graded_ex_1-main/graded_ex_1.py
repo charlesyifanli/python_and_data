3ce8d583cddf66454bd92a40f0050c369f34ced7
Charles <liyifan1400@gmail.com>
2024-09-14 16:18:14 +0800

    update 1409

--- 1409_Graded_ex_1-main/graded_ex_1.py
+++ 1409_Graded_ex_1-main/graded_ex_1.py
@@ -1,4 +1,6 @@
 # Products available in the store by category
+from Demos.win32ts_logoff_disconnected import username
+
 products = {
     "IT Products": [
         ("Laptop", 1000),
@@ -32,38 +34,132 @@ products = {
 
 
 def display_sorted_products(products_list, sort_order):
-    pass
+    judge = False if sort_order == 1 else True
+    return sorted(products_list, key=lambda x: x[1], reverse=judge)
 
 
 def display_products(products_list):
-    pass
+    i = 0
+    for product, price in products_list:
+        i += 1
+        print(f'{i}. {product}:{price}')
 
 
 def display_categories():
-    pass
+    i = 0
+    for key in products.keys():
+        i += 1
+        print(f'{i}. {key}')
 
 
 def add_to_cart(cart, product, quantity):
-    pass
+    category_ = product[0]
+    product_name, product_price = products[category_][product[1] - 1]
+    if not cart.get(product_name):
+        cart[product_name] = [product_price, quantity]
+    else:
+        cart[product_name][quantity] += quantity
+    print('Your product has already been added')
+
 
 def display_cart(cart):
-    pass
+    cost = 0
+    print('Product Name :: Price :: Quantity')
+    for product_name, list_ in cart.items():
+        cost += list_[0] * list_[1]
+        print(f'{product_name} :: {list_[0]} :: {list_[1]}')
+    return cost
 
 
 def generate_receipt(name, email, cart, total_cost, address):
     pass
 
 
-def validate_name(name):
-    pass
+def validate_name(name: str) -> bool:
+    if ' ' not in name:
+        print('Invalid name')
+        return False
+    res = True
+    first, last = name.split(' ')
+    if not first.isalpha() or not last.isalpha():
+        print('Invalid')
+        res = False
+    return res
 
-def validate_email(email):
-    pass
+
+def validate_email(email: str) -> bool:
+    if '@' not in email:
+        print('Invalid email')
+        return False
+    else:
+        return True
+
+
+def get_category(num: int) -> str:
+    i = 0
+    for key in products.keys():
+        i += 1
+        if num == i:
+            return key
+    return ''
 
 
 def main():
-    pass
-    
+    # name
+    username = ''
+    while True:
+        username = input('Please enter your firstname and lastname seperated by blank with only alphabets >> ')
+        if validate_name(username):
+            break
+    # email
+    user_email = ''
+    while True:
+        user_email = input('Please enter your email >> ')
+        if validate_email(user_email):
+            break
+    print(username, user_email)
+    # # show categories
+    # display_categories()
+    # category_chosen = get_category(int(input('Please enter your number corresponding with category >> ')))
+    # # show products
+    # display_products(products[category_chosen])
+    # # process choice
+    # cart = dict()
+    # while True:
+    #     print('1. Select a product to buy\n'
+    #           '2. Sort the products according to the price.\n'
+    #           '3. Go back to the category selection.\n'
+    #           '4. Finish shopping')
+    #     choice = int(input('Please enter your choice >> '))
+    #     if choice == 1:
+    #         product_id = int(input('Please enter the id >> '))
+    #         quantity = int(input('Please enter the number >> '))
+    #         add_to_cart(cart=cart, product=[category_chosen, product_id], quantity=quantity)
+    #         continue
+    #     elif choice == 2:
+    #         print('1. Ascending\n'
+    #               '2. Descending')
+    #         print(display_sorted_products(products[category_chosen], int(input('Please enter the number'))))
+    #         continue
+    #     elif choice == 3:
+    #         display_categories()
+    #         category_chosen = get_category(int(input('Please enter your number corresponding with category >> ')))
+    #         # show products
+    #         display_products(products[category_chosen])
+    #         continue
+    #     elif choice == 4:
+    #         if not cart:
+    #             print('Thank you for using our portal. '
+    #                   'Hope you buy something from us next time. Have a nice day')
+    #             break
+    #         else:
+    #             cost = display_cart(cart)
+    #             print(f'Totally cost: {cost}')
+    #             # delivery
+    #             address = input('Please enter your delivery address')
+    #
+    #             continue
+
 
 """ The following block makes sure that the main() function is called when the program is run. 
 It also checks that this is the module that's being run directly, and not being used as a module in some other program. 
