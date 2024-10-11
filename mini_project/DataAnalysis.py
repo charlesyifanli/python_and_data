cefe719b8afb8c12e63803ce33486670abc76fe7
Charles <liyifan1400@gmail.com>
2024-10-11 22:06:46 +0800

    1011

--- mini_project/DataAnalysis.py
+++ mini_project/DataAnalysis.py
@@ -1,18 +1,34 @@
-import pandas as pd
+from scipy import stats
 
 
 class DataAnalysis:
     def __init__(self) -> None:
         self.df = None
 
-    def list_column_types(self) -> dict:
-        column_types = dict()
-        for column in self.df.columns:
-            column_types[column] = self.get_column_type(column)
-        return column_types
+    def check_normality(self, data) -> dict:
+        n = len(data)
 
-    def get_column_type(self, column: str) -> str:
-        if pd.api.types.is_numeric_dtype(self.df[column]):
-            return 'numeric ordinal' if self.df[column].nunique() < 10 else 'interval'
+        if n < 50:
+            stat, p_value = stats.shapiro(data)
+            test_name = 'Shapiro-Wilk'
         else:
-            return 'non-numeric ordinal' if self.df[column].nunique() < 10 else 'nominal'
+            stat, critical_values, significance_level = stats.anderson(data, dist='norm')
+            p_value = self.get_ad_p_value(stat, critical_values)
+            test_name = 'Anderson-Darling'
+
+        return {
+            'test': test_name,
+            'statistic': stat,
+            'p_value': p_value,
+            'is_normal': p_value > 0.05
+        }
+
+    def get_ad_p_value(self, statistic, critical_values) -> float:
+        """according to Anderson-Darling statistic and critical_values judge p value"""
+        if statistic < critical_values[0]:  # 1% significance level
+            return 1.0
+        elif statistic < critical_values[1]:  # 5% significance level
+            return 0.05
+        elif statistic < critical_values[2]:  # 10% significance level
+            return 0.01
+        return 0.0  # If the statistic exceeds all critical values, the p-value is approximately 0
